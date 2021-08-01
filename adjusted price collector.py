ae90e5d4f57b58352bbb9392c571539fc3962eac
DonggeonMoon <83414846+DonggeonMoon@users.noreply.github.com>
2021-08-01 18:52:43 +0900

    Update adjusted price collector.py

--- adjusted price collector.py
+++ adjusted price collector.py
@@ -1,11 +1,28 @@
-from initialize import *
-import os, re, time
+from connect_info import *
+import os, re, time, pymysql, traceback
 import urllib.request
 from multiprocessing import Pool
 from functools import partial
 
+def connect_db():
+    
+    conn = pymysql.connect(host=connect_info["host"], user=connect_info["user"], password=connect_info["password"], db=connect_info["db"], charset=connect_info["charset"])
+    return conn
+
 def adjustedStockPrice(number, date):
-    df = initialize("종목정보")
+
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute("select stock_code, stock_name, corp_cls from stock_info")
+    df = []
+    while True:
+        row = cur.fetchone()
+        if row == None:
+            break
+        df.append([row[0], row[1], row[2]])
+        
+    conn.close()
+    
     total_number = len(df)
     start_number = int(total_number / 4 * (number - 1))
     end_number = int(total_number / 4 * number)
@@ -18,12 +35,11 @@ def adjustedStockPrice(number, date):
             data = str(urllib.request.urlopen("https://fchart.stock.naver.com/sise.nhn?symbol=" + df[i][0] + "&timeframe=day&count=3000&requestType=0").read())
             price = re.search(r'' + date + '\|[0-9]+\|[0-9]+\|[0-9]+\|[0-9]+', data).group()
             price2 = re.search(r'\|[0-9]+$', price).group().strip('|')
-            df[i] = df[i][:3]
             df[i].append(price2)
             print(df[i])
             
         except:
-            df[i] = df[i][:3]
+            df[i] = df[i]
             print("error")
             continue
             
@@ -36,18 +52,20 @@ if __name__=='__main__':
     pool = Pool(int(os.environ['NUMBER_OF_PROCESSORS']))
     func = partial(adjustedStockPrice, date=date)
     result = pool.map(func, list(range(1, int(os.environ['NUMBER_OF_PROCESSORS']) + 1)))
+
+    conn =  connect_db()
+    cur = conn.cursor()
+    try:
+        for i in result:
+            for j in i:
+                print(j)
+                if(len(j) == 3):
+                    continue
+                sql = "insert into stock_price values('{}', '{}', '{}', '{}', {})".format(j[0], j[1], j[2], date, j[3])
+                cur.execute(sql)
+    except:
+        traceback.print_stack()
+        traceback.print_exc()
     
-    #수정주가YYYYMMDD.txt 파일에 저장
-    f = open("수정주가" + str(date) + ".txt",'w')
-    length = len(result)
-    for i in range(0, length):
-        length2 = len(result[i])
-        for j in range(0,length2):
-            length3 = len(result[i][j])
-            for k in range(0, length3):
-                f.write(result[i][j][k])
-                f.write('\t')
-            f.write("\n")
-    f.close()
-    print("--- %s초 ---" % (time.time() - start_time))
-    #os.system("shutdown -s -t 60")
+    conn.commit()
+    conn.close()
