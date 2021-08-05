3e492193e5a4a8cdc89c4539238ddf87fb7d1328
DonggeonMoon <83414846+DonggeonMoon@users.noreply.github.com>
2021-08-05 22:46:44 +0900

    Update stock_info collector.py

--- stock_info collector.py
+++ stock_info collector.py
@@ -60,5 +60,4 @@ for row in df:
                                                                                
 conn.commit()
 conn.close()
-                                                                               
 print("작업 완료")
