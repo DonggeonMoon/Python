4ce1e7366cded8fdd97b42be1e1285e68235d23c
DonggeonMoon <83414846+DonggeonMoon@users.noreply.github.com>
2021-08-05 22:42:07 +0900

    Update naver finance scraper.py

--- naver finance scraper.py
+++ naver finance scraper.py
@@ -18,8 +18,8 @@ cur = conn.cursor()
 cur.execute("select stock_code, stock_name from stock_info")
 
 stock_info = cur.fetchall()
-print("rows of data: " + str(len(stock_info)))
 conn.close()
+print("number of rows: " + str(len(stock_info)))
 
 df1 = []
 df2 = []
@@ -32,32 +32,30 @@ for row in stock_info:
         data1 = soup.select(".cmp_comment > ul > li")
         data2 = soup.select("#cTB13 > tbody > tr > td:nth-child(1)")
         data3 = soup.select("#cTB13 > tbody > tr > td:nth-child(3)")
-
-        print(row[1], '\n')
-
+        print(row[1])
+        
         # 기업개요 추출
         overview = []
-        for i in range(0, len(data1)):
+        for i in range(len(data1)):
             overview.append(data1[i].text)
+            
         overview = ' '.join(overview).replace('\'', '\\\'')
-
         df1.append([row[0], overview])
-
         print(overview)
-
+        
         # 지분율 추출
         for i, j in zip(data2, data3):
             if i.attrs['title'] != '':
                 share = [row[0], row[1], datetime.date.today().strftime("%y%m%d"), i.attrs['title'], j.text.strip()]
                 df2.append(share)
                 print(share)
-
+                
             else:
                 continue
-
+            
     except:
         continue
-
+    
 # DBMS에 저장
 conn = connect_db()
 cur = conn.cursor()
@@ -67,12 +65,12 @@ for row in df1:
         sql = "update stock_info set overview = '{}' where stock_code = '{}'".format(str(row[1]), str(row[0]))
         print(sql)
         cur.execute(sql)
-
+        
     except:
         traceback.print_stack()
         traceback.print_exc()
         continue
-
+    
 for row in df2:
     try:
         if len(row[4]) == 0:
@@ -83,11 +81,11 @@ for row in df2:
                 str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]))
         print(sql)
         cur.execute(sql)
-
+        
     except:
         traceback.print_stack()
         traceback.print_exc()
         continue
-
+    
 conn.commit()
 conn.close()
