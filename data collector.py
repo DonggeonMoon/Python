3fbdd7c9623843961b0043f666ccec955b3101ac
DonggeonMoon <83414846+DonggeonMoon@users.noreply.github.com>
2021-08-10 00:03:08 +0900

    Update data collector.py

--- data collector.py
+++ data collector.py
@@ -8,26 +8,26 @@ import traceback
 
 def reprt_code_chk():
     quarter = str(input("보고서 종류를 입력하세요(0:사업보고서, 1:1분기보고서, 2:반기보고서, 3:3분기보고서):"))
-    if quarter == "0":
-        return "11011"
-    elif quarter == "1":
-        return "11013"
-    elif quarter == "2":
-        return "11012"
-    elif quarter == "3":
-        return "11014"
+    if quarter == '0':
+        return '11011'
+    elif quarter == '1':
+        return '11013'
+    elif quarter == '2':
+        return '11012'
+    elif quarter == '3':
+        return '11014'
     else:
         print("잘못 입력하셨습니다.")
         return reprt_code_chk()
 
     
 def connect_db():
-    conn = pymysql.connect(host=connect_info["host"], user=connect_info["user"], password=connect_info["password"],
-                           db=connect_info["db"], charset=connect_info["charset"])
+    conn = pymysql.connect(host=connect_info['host'], user=connect_info['user'], password=connect_info['password'],
+                           db=connect_info['db'], charset=connect_info['charset'])
     return conn
 
 
-crtfc_key = connect_info["crtfc_key"]  # API 인증키(openapi.dart.or.kr에서 발급)
+crtfc_key = connect_info['crtfc_key']  # API 인증키(openapi.dart.or.kr에서 발급)
 bsns_year = str(input("연도를 입력하세요:"))  # 사업연도
 reprt_code = reprt_code_chk()  # 보고서 코드
 
@@ -37,7 +37,7 @@ cur = conn.cursor()
 cur.execute("select stock_code, stock_name, corp_cls, corp_code from stock_info")
 
 stock_info = cur.fetchall()
-print("rows of data: " + str(len(stock_info)))
+print('rows of data: ' + str(len(stock_info)))
 conn.close()
 
 # 기초 자료 수집
@@ -48,21 +48,21 @@ for i, j, k, l in stock_info:
         stock_name = j
         corp_cls = k
         corp_code = l
-        print("stock_name:", j, "corp_cls:", k)
-        api = "https://opendart.fss.or.kr/api/empSttus.json?crtfc_key={crtfc_key}" \
-              "&corp_code={corp_code}" \
-              "&bsns_year={bsns_year}" \
-              "&reprt_code={reprt_code}"
+        print('stock_name:', j, 'corp_cls:', k)
+        api = 'https://opendart.fss.or.kr/api/empSttus.json?crtfc_key={crtfc_key}' \
+              '&corp_code={corp_code}' \
+              '&bsns_year={bsns_year}' \
+              '&reprt_code={reprt_code}'
         url = api.format(crtfc_key=crtfc_key, corp_code=corp_code, bsns_year=bsns_year, reprt_code=reprt_code)
         print(url)
         data = json.loads(requests.get(url).text)
         
         num_total = 0
-        for num in data["list"]:
-            if "합계" in num.get("fo_bbm") or "총계" in num.get("fo_bbm"):
+        for num in data['list']:
+            if '합계' in num.get('fo_bbm') or '총계' in num.get('fo_bbm'):
                 continue
-            num = num.get("sm")
-            num = int(num.replace(',', '')) if num != "-" else 0
+            num = num.get('sm')
+            num = int(num.replace(',', '')) if num != '-' else 0
             num_total += num
             print(num_total)
             
